d1d85bd5bf9a82d1908abdd72947fd2c239d1b06
Greg Hermo <greg.hermo@gmail.com>
2021-02-09 11:27:43 +0800

    Install laravel breeze for login and registration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,3 @@
 require('./bootstrap');
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import store from './store'
-
-Vue.use(VueRouter)
-
-
-//Main pages
-import App from './Views/App.vue'
-import Home from "./Views/Home";
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-    ],
-});
-
-const app = new Vue({
-    el: '#app',
-    components: { App },
-    router,
-    store
-});
+require('alpinejs');
