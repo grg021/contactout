f61d083dc843ae2606ce0a0461789c3cb6bb4970
Greg Hermo <greg.hermo@gmail.com>
2021-02-09 10:50:35 +0800

    Setup vue, vue router and vuex

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Home Count - {{ count }}</h1>
+    <button @click="count++">Increment Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+
+  },
+  computed: {
+    count: {
+      get () {
+        return this.$store.getters.count
+      },
+      set (value) {
+        this.$store.commit('increment')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
